fe3ba0a9906408bf5fcf1627c51b40521e4f4cf6
alexisnwokoye@gmail.com <alexisnwokoye@The-Mansion-1.home>
2020-06-09 18:19:41 +0200

    A jour, Reaveal

--- webdesign_portfolio/main.js
+++ webdesign_portfolio/main.js
@@ -1,1 +1,6 @@
-// ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.reveal1');
+ScrollReveal().reveal('.reveal2', { delay: 200 });
+ScrollReveal().reveal('.reveal3', { delay: 300 });
+ScrollReveal().reveal('.reveal4', { delay: 400 });
+ScrollReveal().reveal('.reveal5', { delay: 500 });
+ScrollReveal().reveal('.reveal6', { delay: 600 });
